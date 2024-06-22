118ad33258ea4bfd38e79b176832ff2d002cd546
Dean Santos <dean@santos.ws>
2024-06-22 20:06:20 +1000

    Move editor code to its own javascript file.

---
+++ public/editor.js
@@ -0,0 +1,67 @@
+require.config({ paths: { 'vs': 'https://cdn.bootcdn.net/ajax/libs/monaco-editor/0.20.0/min/vs' } });
+
+require(['vs/editor/editor.main'], function () {
+
+    monaco.languages.register({ id: 'moocode' });
+
+    monaco.languages.setMonarchTokensProvider('moocode', {
+        tokenizer: {
+            root: [
+                // Keywords
+                // comment starts with //
+                [/\/\/.*$/, "comment"],
+                [/\b(if|while|for|return|endif|endwhile|endfor|else|elseif|in|this|try|except|finally|endtry)\b/, "keyword.control"],
+                [/\b(typeof|toint|tofloat|toobj|tostr|raise)\b/, "keyword.other"],
+                [/\b(INT|NUM|FLOAT|LIST|MAP|STR|ANON|OBJ|ERR)\b/, "constant.numeric"],
+                [/@(program|args|verb)\b/, "markup.heading"],
+                // Special handling for object:verb
+                // Capture object and verb separately
+                [/(\w+)([:])(\w+)\b/, ["object", "delimiter", "verb"]],
+                // Special handling for object.property
+                // Capture object and property separately
+                [/(\w+)([.]\w+)\b/, ["object", "property"]],
+                // special handling for object_ref:verb
+                // Capture object_ref and verb separately
+                [/$(\w+)([:])(\w+)\b/, ["object_ref", "delimiter", "verb"]],
+                // special handling for object_ref.property
+                // Capture object_ref and property separately
+                [/$(\w+)([.]\w+)\b/, ["object_ref", "property"]],
+                // Special handling for object_ref
+                [/$(\w+)\b/, "object_ref"],
+                // Special handling for object
+                [/\b(\w+)\b/, "object"],
+
+                [/\b(?:\d+(?:\.\d*)?|\.\d+)\b/, "constant.numeric.moo"],
+                // Strings
+                [/"/, { token: "string.quoted.double.moo", next: "@string" }],
+            ],
+            string: [
+                [/[^\\"]+/, "string"],
+                [/\\./, "constant.character.escape.moo"],
+                [/"/, { token: "string.quoted.double.moo", next: "@pop" }],
+            ]
+        }
+    });
+
+    monaco.editor.defineTheme("moocode", {
+        base: 'vs-dark',
+        inherit: true,
+        rules: [
+
+            { token: 'property', foreground: '2596be' },
+            { token: 'object', foreground: '76b5c5' },
+            // green for verbs
+            { token: 'verb', foreground: 'a6e22e' },
+            // yellow for perenthesis
+            { token: 'delimiter.parenthesis', foreground: 'f8f8f2' },
+            // light purple for object_ref
+            { token: 'object_ref', foreground: 'bd93f9' },
+
+        ],
+    })
+    editor = monaco.editor.create(document.getElementById('SdWiqHtqa'), {
+        value: "",
+        language: 'moocode',
+        theme: "moocode",
+    });
+});
