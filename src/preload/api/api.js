b2c2b79f95fa453a2bbd51d3d88353f60aff4b3f
Dean Santos <dean@santos.ws>
2024-07-05 08:53:20 +1000

    Fix unprintable characters being rendered.

--- src/preload/api/api.js
+++ src/preload/api/api.js
@@ -107,6 +107,9 @@ client.on("data", (data) => {
                 break;
             }
             let line = buffer + data.toString("utf-8", 0, index);
+            // remove any characters that arent typable characters
+            line = line.replace(/[^ -~]/g, "");
+
             ipcRenderer.send("received-data", line);
             data = data.slice(index + 1);
             buffer = "";
